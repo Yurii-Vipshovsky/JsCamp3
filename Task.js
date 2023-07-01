23da20e24beeef69877a8f5c71888593ad64967e
Yurii-Vipshovsky <vip.cool.ua@gmail.com>
2023-07-01 17:30:01 +0300

    Done till 5.2

--- Task.js
+++ Task.js
@@ -50,5 +50,52 @@ console.log(IsEndOfString("abcd", "d"));
 //task 4
 
 function NextArifmetickMean(array){
-    
+    let res = [];
+    for(let i=0;i<array.length-1;++i){
+        res.push((array[i]+array[i+1])/2);
+    }
+    return res;
 }
+
+console.log(NextArifmetickMean([2, -2, 2, -2, 2]));
+console.log(NextArifmetickMean([1, 3, 5, 1, -10]));
+
+//task 5
+
+function countVowels(string){
+    let vovels = ['a', 'e', 'i', 'o', 'u'];
+    string = string.toLowerCase();
+    let count = 0;
+    for(let i=0;i<string.length;++i){
+        if(vovels.indexOf(string[i])!=-1){
+            count++;
+        }
+    }
+    return count
+}
+
+console.log(countVowels("Celebration"));
+console.log(countVowels("Palm"));
+
+//task 5.2
+
+function removeABC(string){
+    let ABCSearch = ['a','b','c'];
+    let searchString = string.toLowerCase();
+    let isDeleted = false;
+    for(let i=0;i<ABCSearch.length;++i){
+        while(searchString.indexOf(ABCSearch[i])!=-1){
+            let index = searchString.indexOf(ABCSearch[i]);
+            searchString = searchString.slice(0, index) + searchString.slice(index+1);
+            string = string.slice(0, index) + string.slice(index+1);
+            isDeleted=true;
+        }
+    }
+    if(!isDeleted){
+        return null;
+    }
+    return string;
+}
+
+console.log(removeABC("This might be a bit hard"));
+console.log(removeABC("hello world!"));
